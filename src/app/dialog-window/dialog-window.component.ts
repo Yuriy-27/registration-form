e3952bf1612f3fc9ea1817741eadf4d7212789e9
Yuriy-27 <yuriyshchebetun@gmail.com>
2019-11-08 19:20:14 +0200

    added dialog component

---
+++ src/app/dialog-window/dialog-window.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-dialog-window',
+  templateUrl: './dialog-window.component.html',
+  styleUrls: ['./dialog-window.component.scss']
+})
+export class DialogWindowComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogWindowComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  ngOnInit() {
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+
+}
